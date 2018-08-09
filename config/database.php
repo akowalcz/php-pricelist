25e4ca68c25bacc771e3cf519ffc837f61e9385f
Jaroslaw Stakun <jstakun.appspot@gmail.com>
2018-08-09 14:23:42 +0200

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,13 +1,10 @@
 <?php
 // used to connect to the database
-$host = getenv("MYSQL_SERVICE_HOST");
-$port = getenv("MYSQL_SERVICE_PORT");
-$db_name = getenv("database_name");
-$username = getenv("username");
-$password = getenv("password");
-//$db_name = getenv("MYSQL_DATABASE");
-//$username = getenv("MYSQL_USER");
-//$password = getenv("MYSQL_PASSWORD");
+$host = getenv("DB_HOST");
+$port = getenv("DB_PORT");
+$db_name = getenv("DB_NAME");
+$username = getenv("DB_USER");
+$password = getenv("DB_PASSWORD");
 
 try {
 	$con = new PDO("mysql:host={$host};dbname={$db_name};port={$port}", $username, $password);
